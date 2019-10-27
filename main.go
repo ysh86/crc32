fb62f12b5940d96d4057c3e04a01dce42e8ee4d2
Yoshiharu <ysh86@users.noreply.github.com>
2019-10-27 22:15:03 +0900

    refactoring

--- main.go
+++ main.go
@@ -8,28 +8,31 @@ import (
 	"os"
 )
 
-func hash_file_crc32(filePath string, polynomial uint32) (string, error) {
-	var returnCRC32String string
-	file, err := os.Open(filePath)
+func hashFileCRC32(filePath string, polynomial uint32) (string, error) {
+	fin, err := os.Open(filePath)
 	if err != nil {
-		return returnCRC32String, err
+		return "", err
 	}
-	defer file.Close()
+	defer fin.Close()
+
 	tablePolynomial := crc32.MakeTable(polynomial)
 	hash := crc32.New(tablePolynomial)
-	if _, err := io.Copy(hash, file); err != nil {
-		return returnCRC32String, err
+	if _, err = io.Copy(hash, fin); err != nil {
+		return "", err
 	}
-	hashInBytes := hash.Sum(nil)[:]
-	returnCRC32String = hex.EncodeToString(hashInBytes)
-	return returnCRC32String, nil
 
+	hashInBytes := hash.Sum(nil)[:]
+	hashString := hex.EncodeToString(hashInBytes)
+
+	return hashString, nil
 }
 
 func main() {
 	filePath := os.Args[1]
-	hash, err := hash_file_crc32(filePath, 0xedb88320)
-	if err == nil {
-		fmt.Printf("%s %s\n", filePath, hash)
+	hash, err := hashFileCRC32(filePath, 0xedb88320)
+	if err != nil {
+		panic(err)
 	}
+
+	fmt.Printf("%s: %s\n", filePath, hash)
 }
